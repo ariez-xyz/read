bf178334dd655da3b77eb6561dcb0e488882253c
ariez-xyz <dpape39@icloud.com>
2022-08-26 10:46:09 +0200

    crudely refactor as a class

--- read.py
+++ read.py
@@ -7,89 +7,93 @@ from tkinter import ttk
 from tkinterweb import HtmlFrame
 import xml.etree.ElementTree as ET
 
-from ctypes import windll
-windll.shcore.SetProcessDpiAwareness(1) # Fix blurry scaling for HiDPI on Windows
+class Read:
+    def __init__(self):
+        self.current_spine_item = 0
+        self.appdata_dir = (os.path.join(os.getenv("APPDATA"), "read.py"))
+        try: # first launch
+            os.mkdir(self.appdata_dir)
+        except FileExistsError:
+            pass
 
-current_spine_item = 0
+        # extract epub contents to appdata directory, unless already present.
+        with open(sys.argv[1], "rb") as file:
+            self.book_hash = hashlib.md5(file.read()).hexdigest() # use md5 as ID
+            try:
+                self.cache_path = os.path.join(self.appdata_dir, self.book_hash)
+                os.mkdir(self.cache_path)
+                with zipfile.ZipFile(sys.argv[1]) as epub:
+                    epub.extractall(self.cache_path)
+            except FileExistsError:  # work already done
+                pass
 
-def namespace(el):
-    return el.tag.split('}')[0].strip('{')
+        self.root = ET.parse(os.path.join(self.cache_path, 'META-INF', 'container.xml'))
+
+        # Find .opf file describing the structure of the epub.
+        for el in self.root.findall('.//*[@media-type]'):
+            if el.attrib['media-type'] == 'application/oebps-package+xml':
+                self.full_path = el.attrib['full-path']
+                self.index_dir = os.path.join(self.cache_path, *self.full_path.split("/")[:-1])
+                self.index_path = os.path.join(self.cache_path, *self.full_path.split("/"))
+
+        if not self.index_path:
+            print("Cannot find path to OPF file in container.xml.")
+            exit(1)
             
-def child_with_id(id, el):
-    matching_items = el.findall(f".//*[@id='{id}']")
-    if len(matching_items) != 1:
-        print(f"Cannot find manifest item for idref {idref}!")
-        exit(1)
-    return matching_items[0]
-    
-# Get the path to a spine item
-def get_path(idref, manifest):
-    href = child_with_id(idref, manifest).attrib['href']
-    return pathname2url(os.path.join(index_dir, *href.split("/")))
-    
-def load_prev():
-    global current_spine_item
-    if current_spine_item > 0:
-        current_spine_item -= 1
-    html_frame.load_file(get_path(spine[current_spine_item], manifest_el))
+        # Find manifest and spine
+        self.root = ET.parse(self.index_path).getroot()
+        self.manifest_el = self.root.find(f".//{{{self.namespace(self.root)}}}manifest")
+        self.spine_el = self.root.find(f".//{{{self.namespace(self.root)}}}spine")
 
-def load_next():
-    global current_spine_item
-    if current_spine_item < len(spine) - 1:
-        current_spine_item += 1
-    html_frame.load_file(get_path(spine[current_spine_item], manifest_el))
-            
-appdata_dir = (os.path.join(os.getenv("APPDATA"), "read.py"))
-try: # first launch
-    os.mkdir(appdata_dir)
-except FileExistsError:
-    pass
+        self.spine = [x.attrib['idref'] for x in self.spine_el] # List of idrefs giving the book contents
+        self.book_title = self.child_with_id('title', self.root).text #
 
-# extract epub contents to appdata directory, unless already present.
-with open(sys.argv[1], "rb") as file:
-    book_hash = hashlib.md5(file.read()).hexdigest() # use md5 as ID
-    try:
-        cache_path = os.path.join(appdata_dir, book_hash)
-        os.mkdir(cache_path)
-        with zipfile.ZipFile(sys.argv[1]) as epub:
-            epub.extractall(cache_path)
-    except FileExistsError:  # work already done
-        pass
+        self.root = Tk()
+        self.root.title(self.book_title)
 
-root = ET.parse(os.path.join(cache_path, 'META-INF', 'container.xml'))
+        self.mainframe = ttk.Frame(self.root, padding="3 3 12 12")
+        self.mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        self.root.columnconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=1)
 
-# Find .opf file describing the structure of the epub.
-for el in root.findall('.//*[@media-type]'):
-    if el.attrib['media-type'] == 'application/oebps-package+xml':
-        full_path = el.attrib['full-path']
-        index_dir = os.path.join(cache_path, *full_path.split("/")[:-1])
-        index_path = os.path.join(cache_path, *full_path.split("/"))
+        self.html_frame = HtmlFrame(self.mainframe, horizontal_scrollbar="auto", messages_enabled=False)
+        self.html_frame.load_file(self.get_path(self.current_spine_item))
+        self.html_frame.grid(column=1, row=1)
 
-if not index_path:
-    print("Cannot find path to OPF file in container.xml.")
-    exit(1)
-    
-# Find manifest and spine
-root = ET.parse(index_path).getroot()
-manifest_el = root.find(f".//{{{namespace(root)}}}manifest")
-spine_el = root.find(f".//{{{namespace(root)}}}spine")
+        ttk.Button(self.mainframe, text="Prev", command=self.load_prev).grid(column=1, row=2, sticky=W)
+        ttk.Button(self.mainframe, text="Next", command=self.load_next).grid(column=2, row=2, sticky=E)
 
-spine = [x.attrib['idref'] for x in spine_el] # List of idrefs giving the book contents
-book_title = child_with_id('title', root).text #
+        self.root.mainloop()
 
-root = Tk()
-root.title(book_title)
+    def namespace(self, el):
+        return el.tag.split('}')[0].strip('{')
+                
+    def child_with_id(self, id, el):
+        matching_items = el.findall(f".//*[@id='{id}']")
+        if len(matching_items) != 1:
+            print(f"Cannot find manifest item for idref {self.idref}!")
+            exit(1)
+        return matching_items[0]
+        
+    # Get the path to a spine item
+    def get_path(self, item):
+        idref = self.spine[item]
+        href = self.child_with_id(idref, self.manifest_el).attrib['href']
+        return pathname2url(os.path.join(self.index_dir, *href.split("/")))
+        
+    def load_prev(self):
+        if self.current_spine_item > 0:
+            self.current_spine_item -= 1
+        self.html_frame.load_file(self.get_path(self.current_spine_item))
 
-mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
+    def load_next(self):
+        if self.current_spine_item < len(self.spine) - 1:
+            self.current_spine_item += 1
+        self.html_frame.load_file(self.get_path(self.current_spine_item))
+                
 
-html_frame = HtmlFrame(mainframe, horizontal_scrollbar="auto", messages_enabled=False)
-html_frame.load_file(get_path(spine[current_spine_item], manifest_el))
-html_frame.grid(column=1, row=1)
+if __name__ == "__main__":
+    from ctypes import windll
+    windll.shcore.SetProcessDpiAwareness(1) # Fix blurry scaling for HiDPI on Windows
 
-ttk.Button(mainframe, text="Prev", command=load_prev).grid(column=1, row=2, sticky=W)
-ttk.Button(mainframe, text="Next", command=load_next).grid(column=2, row=2, sticky=E)
-
-root.mainloop()
+    Read()
