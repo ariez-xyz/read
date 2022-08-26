abdd23d8e9f2b0e45c659b53d53dff01f482be5b
ariez-xyz <dpape39@icloud.com>
2022-08-26 10:37:45 +0200

    basic interface

--- read.py
+++ read.py
@@ -2,13 +2,15 @@
 
 import hashlib, sys, os, zipfile
 from nturl2path import pathname2url
-import tkinter as tk
+from tkinter import *
 from tkinter import ttk
 from tkinterweb import HtmlFrame
 import xml.etree.ElementTree as ET
 
 from ctypes import windll
 windll.shcore.SetProcessDpiAwareness(1) # Fix blurry scaling for HiDPI on Windows
+
+current_spine_item = 0
 
 def namespace(el):
     return el.tag.split('}')[0].strip('{')
@@ -25,6 +27,17 @@ def get_path(idref, manifest):
     href = child_with_id(idref, manifest).attrib['href']
     return pathname2url(os.path.join(index_dir, *href.split("/")))
     
+def load_prev():
+    global current_spine_item
+    if current_spine_item > 0:
+        current_spine_item -= 1
+    html_frame.load_file(get_path(spine[current_spine_item], manifest_el))
+
+def load_next():
+    global current_spine_item
+    if current_spine_item < len(spine) - 1:
+        current_spine_item += 1
+    html_frame.load_file(get_path(spine[current_spine_item], manifest_el))
             
 appdata_dir = (os.path.join(os.getenv("APPDATA"), "read.py"))
 try: # first launch
@@ -64,13 +77,19 @@ spine_el = root.find(f".//{{{namespace(root)}}}spine")
 spine = [x.attrib['idref'] for x in spine_el] # List of idrefs giving the book contents
 book_title = child_with_id('title', root).text #
 
-root = tk.Tk()
+root = Tk()
 root.title(book_title)
-frame = HtmlFrame(root, horizontal_scrollbar="auto", messages_enabled=False)
 
-frame.load_file(get_path(spine[4], manifest_el))
-frame.pack(fill="both", expand=True) #attach the HtmlFrame widget to the parent window
-#with open("ch13.html", encoding='utf-8') as f:
-#    frame.load_html(f.read())
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+html_frame = HtmlFrame(mainframe, horizontal_scrollbar="auto", messages_enabled=False)
+html_frame.load_file(get_path(spine[current_spine_item], manifest_el))
+html_frame.grid(column=1, row=1)
+
+ttk.Button(mainframe, text="Prev", command=load_prev).grid(column=1, row=2, sticky=W)
+ttk.Button(mainframe, text="Next", command=load_next).grid(column=2, row=2, sticky=E)
 
 root.mainloop()
